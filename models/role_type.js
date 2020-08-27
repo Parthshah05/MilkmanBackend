cd6759049f4d6880a6d608dc62270aa109aa0f36
parthshah05 <parth.babu05@gmail.com>
2020-08-27 12:45:58 +0530

    Final Commit

--- models/role_type.js
+++ models/role_type.js
@@ -6,7 +6,7 @@ const Role_typeSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 32,
+      maxlength: 34,
       unique: true
     }
   }
